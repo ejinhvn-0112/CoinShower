dd99b3ed05264f6acb296edbc394c894c3dd8b57
dodoomdaada@naver.com <dodoomdaada@naver.com>
2021-05-13 16:23:42 +0900

    Initial Commit

---
+++ Coin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coin
+//
+//  Created by 김은중 on 2021/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
